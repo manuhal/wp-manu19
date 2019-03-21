67c69e76c908d102bde5c07102614bfbfe2a32e0
manuyap <manuyap@gmail.com>
2019-03-21 06:14:58 +0000

    set parent & child breadcrumb links

--- page.php
+++ page.php
@@ -1,4 +1,4 @@
- <?php
+<?php
  
    get_header();
  
@@ -17,10 +17,16 @@
   </div>
 
   <div class="container container--narrow page-section">
+      
+    <?php
+      $the_parent = wp_get_post_parent_id(get_the_ID());
+      if($the_parent){
+    ?>
+        <div class="metabox metabox--position-up metabox--with-home-link">
+          <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($the_parent) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($the_parent) ?></a> <span class="metabox__main"><?php the_title() ?></span></p>
+        </div>
+    <?php } ?>
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
-    </div>
     
     <!--
     <div class="page-links">
